1c38413df0b05c7693398626adfced98067946a5
Hector Zarco <hezarco@gmail.com>
2015-08-30 19:03:05 +0200

    Get repo info

--- hello.go
+++ hello.go
@@ -4,6 +4,7 @@ import (
   "github.com/google/go-github/github"
   "github.com/ant0ine/go-json-rest/rest"
   "fmt"
+  "strings"
   "log"
   "net/http"
 )
@@ -13,14 +14,22 @@ func main() {
 
   api.Use(rest.DefaultDevStack...)
 
-  api.SetApp(rest.AppSimple(func(w rest.ResponseWriter, r *rest.Request) {
-    username := "zzarcon"
-    client := github.NewClient(nil)
-    orgs, _, _ := client.Organizations.List(username, nil)
-
-    fmt.Println(orgs)
-    w.WriteJson(map[string]string{"Body": "Hello World!"})
-  }))
-
+  router, err := rest.MakeRouter(
+    rest.Get("/stats/#id", GetStat),
+  )
+  if err != nil {
+      log.Fatal(err)
+  }
+  api.SetApp(router)
   log.Fatal(http.ListenAndServe(":8080", api.MakeHandler()))
+}
+
+func GetStat(w rest.ResponseWriter, r *rest.Request) {
+  id := r.PathParam("id")
+  chunks := strings.Split(id, "-")
+  client := github.NewClient(nil)
+  orgs, _, _ := client.Repositories.Get(chunks[0], chunks[1])
+
+  fmt.Println(orgs)
+  w.WriteJson(orgs)
 }
